f4e74708a02962193dc45e6434a5e9458e619c9c
qwer11121 <kk1985.9@outlook.com>
2022-11-27 22:20:55 +0800

    Adapt for different font size

--- AssemblerQueueInfo.cs
+++ AssemblerQueueInfo.cs
@@ -1,87 +1,103 @@
-        string LCDTag = "AInfo";  //Name of the LCD panel you want the info on
+string LCDTag = "AInfo";  //Name of the LCD panel you want the info on
 
-        //-------- Don't change anything below this line :) ----------------
+//-------- Don't change anything below this line :) ----------------
 
-        List<IMyAssembler> assemblers = new List<IMyAssembler>();
-        List<MyProductionItem> singleQueue = new List<MyProductionItem>();
+List<IMyAssembler> assemblers = new List<IMyAssembler>();
+List<MyProductionItem> singleQueue = new List<MyProductionItem>();
 
-        public struct QueueItem
+Dictionary<string, List<MyProductionItem>> outputDict = new Dictionary<string, List<MyProductionItem>>();
+
+List<IMyTextPanel> Panels = new List<IMyTextPanel>();
+
+public Program()
+{
+    Runtime.UpdateFrequency = UpdateFrequency.Update10 | UpdateFrequency.Update100;
+    GridTerminalSystem.GetBlocksOfType(assemblers);
+    UpdatePanels();
+}
+
+public void Main(string argument, UpdateType updateSource)
+{
+    if ((updateSource & UpdateType.Update100) != 0)
+    {
+        UpdatePanels();
+        assemblers.Clear();
+        GridTerminalSystem.GetBlocksOfType(assemblers);
+    }
+
+    for (int i = 0; i < assemblers.Count; i++)
+    {
+        if (!assemblers[i].IsQueueEmpty)
         {
-            public string Text { get; }
-            public int Number { get; }
-
-            public override string ToString() => $"{Number} x {Text}";
-        }
-
-        Dictionary<string, List<MyProductionItem>> queueDict = new Dictionary<string, List<MyProductionItem>>();
-        Dictionary<string, string> outputDict = new Dictionary<string, string>();
-
-        List<IMyTextPanel> Panels = new List<IMyTextPanel>();
-
-        public Program()
-        {
-            Runtime.UpdateFrequency = UpdateFrequency.Update10 | UpdateFrequency.Update100;
-            GridTerminalSystem.GetBlocksOfType(assemblers);
-            UpdatePanels();
-        }
-
-        public void Main(string argument, UpdateType updateSource)
-        {
-            if ((updateSource & UpdateType.Update100) != 0)
+            string name = assemblers[i].CustomName;
+            assemblers[i].GetQueue(singleQueue);
+            outputDict[name] = new List<MyProductionItem>();
+            if (singleQueue.Count > 0)
             {
-                UpdatePanels();
-                assemblers.Clear();
-                GridTerminalSystem.GetBlocksOfType(assemblers);
-            }
-
-            for (int i = 0; i < assemblers.Count; i++)
-            {
-                if (!assemblers[i].IsQueueEmpty)
+                for (int j = 0; j < singleQueue.Count; j++)
                 {
-                    string name = assemblers[i].CustomName;
-                    assemblers[i].GetQueue(singleQueue);
-                    outputDict[name] = name + "\'s queue:\n";
-                    if (singleQueue.Count > 0)
-                    {
-                        for (int j = 0; j < singleQueue.Count; j++)
-                        {
-                            outputDict[name] += "\n" + singleQueue[j].Amount + " x " +  singleQueue[j].BlueprintId.SubtypeName.Replace("Component","");
-                        }
-                    }
-                    else
-                    {
-                        outputDict[name] += "\n No items in queue";
-                    }
-                }                
-            }    
-
-            foreach (IMyTextPanel panel in Panels)
-            {
-                string text = "---------------------\n";
-                string[] assemblerName = panel.CustomData.Split('\n');
-                foreach (string name in assemblerName)
-                {
-                    if (outputDict.ContainsKey(name))
-                    {
-                        text += outputDict[name];
-                        text += "\n---------------------\n\n";
-                    }                    
-                }
-                panel.WriteText(text);
-            }
-        }
-
-        void UpdatePanels()
-        {
-            List<IMyTextPanel> LCDs = new List<IMyTextPanel>();
-            Panels.Clear();
-            GridTerminalSystem.GetBlocksOfType(LCDs);
-            foreach (IMyTextPanel Panel in LCDs)
-            {
-                if (Panel.CustomName.Contains("[" + LCDTag + "]"))
-                {
-                    Panel.ContentType = ContentType.TEXT_AND_IMAGE;
-                    Panels.Add(Panel);
+                    outputDict[name].Add(singleQueue[j]);
                 }
             }
         }
+    }
+
+    foreach (IMyTextPanel panel in Panels)
+    {
+        // By default there could be 38 charactors in a line
+        int defaultScreenWidth = 38;
+        char dot = '_';
+        long fontCode = panel.GetValue<long>("Font");
+
+        // Monospace Font has only 25 charactors in a line
+        if (fontCode == 1147350002)
+        {
+            defaultScreenWidth = 25;
+            dot = '.';
+        }
+
+        float fontSize = panel.FontSize;
+        int screenWidth = (int)(defaultScreenWidth / fontSize);
+
+        StringBuilder text = new StringBuilder();
+        string[] assemblerName = panel.CustomData.Split('\n');
+        foreach (string name in assemblerName)
+        {
+            if (outputDict.ContainsKey(name))
+            {
+                // output data in outputDict as Text
+                text.Append($"<<{name}>>\n").Append(new string('-', defaultScreenWidth)).Append("\n");
+                foreach (var item in outputDict[name])
+                {
+                    string itemText = item.BlueprintId.SubtypeName.Replace("Component", "");
+                    string itemAmount = item.Amount.ToString();
+                    int dotCount = screenWidth - itemText.Length - itemAmount.Length;
+                    if (dotCount <= 2)
+                        dotCount = 2;
+                    string dots = new string(dot, dotCount);
+                    text.Append(itemText)
+                    .Append(dots)
+                    .Append(itemAmount)
+                    .Append("\n");
+                }
+                text.Append("\n");
+            }
+        }
+        panel.WriteText(text.ToString());
+    }
+}
+
+void UpdatePanels()
+{
+    List<IMyTextPanel> LCDs = new List<IMyTextPanel>();
+    Panels.Clear();
+    GridTerminalSystem.GetBlocksOfType(LCDs);
+    foreach (IMyTextPanel Panel in LCDs)
+    {
+        if (Panel.CustomName.Contains("[" + LCDTag + "]"))
+        {
+            Panel.ContentType = ContentType.TEXT_AND_IMAGE;
+            Panels.Add(Panel);
+        }
+    }
+}
